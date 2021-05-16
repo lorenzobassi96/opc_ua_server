10679bf614b5c4efab2213c91b107f42a12f3f71
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-05-16 23:43:53 +0200

    Create change_time.sh

---
+++ change_time.sh
@@ -0,0 +1,40 @@
+#./change_time.sh (porta) (intervallo pubblicazione)
+
+cd /home/ubuntu
+sudo rm -rf C-Projects
+sudo mkdir /home/ubuntu/C-Projects
+sudo mkdir /home/ubuntu/C-Projects/OPC_UA
+cd temp_opcua_installation_process/
+cd /home/ubuntu/temp_opcua_installation_process/open62541/build/
+sudo cp open62541.* /home/ubuntu/C-Projects/OPC_UA
+cd /home/ubuntu/C-Projects/OPC_UA
+
+sudo git clone https://github.com/lorenzobassi96/opc_ua_server_adaptive.git
+
+cd opc_ua_server_adaptive/
+
+sudo cp myServer.c /home/ubuntu/C-Projects/OPC_UA   #devo copiare il file nella cartella $
+cd /home/ubuntu/C-Projects/OPC_UA
+
+#vado a cambiare il tempo di pubblicazione
+sudo replace "tempo_pubblicazione" $1 -- myServer.c
+
+
+echo "--------------------------------------"
+echo "RE-BUILD OF THE SERVER..."
+echo "--------------------------------------"
+
+gcc -std=c99 open62541.c myServer.c -o myServer
+#sudo gcc myServer.c -o myServer -lopen62541
+
+echo "--------------------------------------"
+echo "STARTING NEW SERVER..."
+echo "--------------------------------------"
+
+sudo cp -R * /home/ubuntu/
+cd /home/ubuntu/
+
+#IP=$(/sbin/ifconfig ens3 | grep 'inet' | cut -d: -f2 | awk $'{print $2}')
+#IP=$(/sbin/ifconfig ens3 | grep inet | awk '{print $2}' | awk  -F : '{print $2}')
+
+./myServer $1 $2
