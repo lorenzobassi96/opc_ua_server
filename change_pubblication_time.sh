2ac8757f575ecb713bc58badc915e132a13d8428
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-05-16 23:27:12 +0200

    Update change_pubblication_time.sh

--- change_pubblication_time.sh
+++ change_pubblication_time.sh
@@ -1,25 +1,45 @@
 kill -9 $(lsof -t -i:$2)
+echo "--------------------------------------"
+echo "SERVER TERMIANTO"
+echo "--------------------------------------"
+IP=$(/sbin/ifconfig ens3 | grep 'inet' | cut -d: -f2 | awk $'{print $2}')
 
 cd /home/ubuntu
-rm -rf C-Projects
-mkdir /home/ubuntu/C-Projects
-mkdir /home/ubuntu/C-Projects/OPC_UA
+sudo rm -rf C-Projects
+sudo mkdir /home/ubuntu/C-Projects
+sudo mkdir /home/ubuntu/C-Projects/OPC_UA
 cd temp_opcua_installation_process/
 cd /home/ubuntu/temp_opcua_installation_process/open62541/build/
-cp open62541.* /home/ubuntu/C-Projects/OPC_UA
+sudo cp open62541.* /home/ubuntu/C-Projects/OPC_UA
 cd /home/ubuntu/C-Projects/OPC_UA
 
 sudo git clone https://github.com/lorenzobassi96/opc_ua_server_adaptive.git
+
 cd opc_ua_server_adaptive/
-cp myServer.c /home/ubuntu/C-Projects/OPC_UA   #devo copiare il file nella cartella di lavoro
+
+sudo cp myServer.c /home/ubuntu/C-Projects/OPC_UA   #devo copiare il file nella cartella $
 cd /home/ubuntu/C-Projects/OPC_UA
 
 #vado a cambiare il tempo di pubblicazione
-replace "tempo_pubblicazione" $1 -- myServer.c
+sudo replace "tempo_pubblicazione" $1 -- myServer.c
+
+
+echo "--------------------------------------"
+echo "RE-BUILD OF THE SERVER..."
+echo "--------------------------------------"
 
 gcc -std=c99 open62541.c myServer.c -o myServer
-cp -R * /home/ubuntu/
-chmod +x myServer
+#sudo gcc myServer.c -o myServer -lopen62541
 
-IP=$(/sbin/ifconfig ens3 | grep 'inet' | cut -d: -f2 | awk $'{print $2}')
+echo "--------------------------------------"
+echo "STARTING NEW SERVER..."
+echo "--------------------------------------"
+
+sudo cp -R * /home/ubuntu/
+cd /home/ubuntu/
+
+#IP=$(/sbin/ifconfig ens3 | grep 'inet' | cut -d: -f2 | awk $'{print $2}')
+#IP=$(/sbin/ifconfig ens3 | grep inet | awk '{print $2}' | awk  -F : '{print $2}')
+
+
 ./myServer $IP $2
