2b5a817e2e2f113abe73f7ee58904db69124fb6f
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-05-18 11:50:51 +0200

    Create connection_to_find_pubb_time.py

---
+++ connection_to_find_pubb_time.py
@@ -0,0 +1,16 @@
+import sys
+from sys import argv
+sys.stdout = open('tempi.txt', 'w')
+ip_address = str(sys.argv[1])
+port_number = str(sys.argv[2])         
+domain = "opc.tcp://"
+ddd = ":"
+final_address = (domain+ip_address+ddd+port_number)
+print (final_address)
+from opcua import Client
+client=Client(final_address)
+client.connect()
+from time import sleep
+while True:
+ print(client.get_node("ns=1;s=Random_Number").get_value())
+ sleep(2)
